2ffcfd29bc448e549fe7c63445b645b3b00e026d
alubetk1 <alubetk1@swarthmore.edu>
2021-04-15 08:29:27 -0700

    update

---
+++ src/components/TopBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/TopBar.css";
+
+const TopBar = () => {
+  return <div></div>;
+};
+
+export default TopBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, Router, browserHistory } from "react-router";
+import TopBar from "./components/TopBar";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
